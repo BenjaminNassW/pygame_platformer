31c64e20299cd040e84c336f51d6bed467cab275
Benjamin Nass <benjanass@gmail.com>
2024-01-09 21:29:38 -0300

    creating map, walking and jumping

--- main.py
+++ main.py
@@ -1,42 +1,140 @@
 import pygame
 import sys
-import mario_sprite
+
 
 pygame.init()
 
 # Set up display
-screen_width, screen_height = 800, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Mario-like Game")
+screen_width, screen_height = 1000, 1000
+screen = pygame.display.set_mode(
+    (screen_width, screen_height), pygame.RESIZABLE)
+pygame.display.set_caption("Platformer")
 
 # Load the sprite sheet
-mario_spritesheet = pygame.image.load(
-    "C:/Users/benja/OneDrive/Desktop/pygame/mario.png").convert_alpha()
+cloud_background = pygame.image.load(
+    "C:/Users/benja/OneDrive/Desktop/pygame/cloud_bg.png").convert_alpha()
 
-# Initial player position
-player_x, player_y = screen_width // 2, screen_height // 2
 
-# Scale factor for the sprite
-scale_factor = 5  # Adjust this value to control the scale
+tile_size = 50
 
-# Player speed
-player_speed = 5
 
-# Create sprite sheets
-sprite_sheet = mario_sprite.SpriteSheet(mario_spritesheet)
-sprite_sheet_walk = mario_sprite.SpriteSheet(mario_spritesheet)
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line *
+                         tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line *
+                         tile_size, 0), (line * tile_size, screen_height))
 
-# Load frames
-frame_idle = sprite_sheet.get_image(209, 14, 20, scale_factor)
-frame_walk = sprite_sheet_walk.get_image(328, 15, 20, scale_factor)
 
-# Initial state
-current_frame = frame_idle
-walking_frames = [frame_walk, frame_idle]
-walk_frame_index = 0
-walk_frame_timer = 0
-walk_frame_delay = 5  # Adjust this value to control the walking animation speed
+class Player():
+    def __init__(self, x, y):
+        self.images_right = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 9):
+            img_right = pygame.image.load(f'img/player/p1_walk0{num}.png')
+            img_right = pygame.transform.scale(img_right, (40,80))
+            self.images_right.append(img_right)
+        
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
 
+    def update(self):
+        dx = 0
+        dy = 0
+
+        # get key presses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped == True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        #handle animation
+        self.index += 1
+        if self.index >= len(self.images_right):
+            self.index = 0
+        self.image = self.images_right[self.index]
+
+        # add gravity
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        # check for collision
+
+        # update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # draw player
+        screen.blit(self.image, self.rect)
+
+
+class World():
+
+    def __init__(self, data):
+        self.tile_list = []
+
+        # load images
+        # dirt_img = pygame.image.load()
+        grass_img = pygame.image.load('grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(
+                        grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+
+world_data = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+world = World(world_data)
+player = Player(100, screen_height - 130)
 
 # Game loop
 clock = pygame.time.Clock()
@@ -47,28 +145,12 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_x > 0:
-        player_x -= player_speed
-        # Switch to walking frame when moving left
-        current_frame = walking_frames[walk_frame_index]
-    elif keys[pygame.K_RIGHT] and player_x < screen_width - sprite_sheet.width * scale_factor:
-        player_x += player_speed
-        # Switch to walking frame when moving right
-        current_frame = walking_frames[walk_frame_index]
-    else:
-        current_frame = frame_idle  # Switch back to idle frame when not moving
-
-    # Update walking frame index and timer
-    walk_frame_timer += 1
-    if walk_frame_timer >= walk_frame_delay:
-        walk_frame_timer = 0
-        walk_frame_index = (walk_frame_index + 1) % len(walking_frames)
- 
     # Draw the sprite onto the screen
-    screen.fill((255, 255, 255))  # Fill the screen with a white background
-    screen.blit(current_frame, (player_x, player_y))
+    screen.fill((148, 165, 255))  # Fill the screen with a white background
+    screen.blit(cloud_background, (0, 0))
+    world.draw()
+    player.update()
+    # draw_grid()
     pygame.display.flip()
 
     clock.tick(60)  # Adjust the frame rate to control animation speed
